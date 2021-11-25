17d5e086276f0d0935b8de292776dbc10e81d15b
CodingLunacy <90979616+CodingLunacy@users.noreply.github.com>
2021-11-25 14:00:53 +0000

    Documentation of software
    Documenting python program

--- RLE Encoding.py
+++ RLE Encoding.py
@@ -1,4 +1,21 @@
-def encode(characters, encoding=[]):
+# Python 3.8.3
+def encode(characters: str, encoding:"previous-encoding"=[]) -> str:
+    """Intakes a string, and compresses using RLE encoding returning a list.
+    
+    Parameters
+    ----------
+    characters: str
+        The string which you wish to compress
+    encoding: list
+        Previous encoding of the function
+        
+    Returns
+    -------
+    list:
+        A 2D list, which includes [character, occurences]
+        for each compressed character
+        
+    """
     key = [None, 0]
     for i in range(len(characters)):
         if not key[0]:
@@ -11,6 +28,7 @@ def encode(characters, encoding=[]):
     encoding.append(key)
     return encoding
 
-while True:
-    compressable_text = list(input("Enter text to be compressed! \n:"))
-    print(" ".join([str(i[0])+" "+str(i[1]) for i in encode(compressable_text)]))
+if __name__ == "__main__":
+    while True:
+        compressable_text = list(input("Enter text to be compressed! \n:"))
+        print(" ".join([str(i[0])+" "+str(i[1]) for i in encode(compressable_text)]))
